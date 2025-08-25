52a42d29f23f079d1a685338d024860cf68d0040
Keyboard1000n17 <keyboard1000n17@gmail.com>
2025-08-25 23:06:47 +0300

    Modified README, added front matter to WinPedia pages.
    Apparently I didn't have to waste so much time manually removing structural tags and front matter on WinPedia pages. I could've just used ex commands but oh well, you learn as you go. I won't have to waste too much time on WinPedia pages.

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,7 +4,7 @@ const eleventySass = require("eleventy-sass");
 // because I don't know bullshit, so,
 // very sorry to y'all who hate vibe coding.
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass, {
     sass: {
       loadPaths: ["node_modules"],
@@ -18,22 +18,21 @@ module.exports = function(eleventyConfig) {
       },
       get order() {
         return this.order;
-      }
-    }
+      },
+    },
   });
 
-  eleventyConfig.addCollection("macpedia", function(collection) {
+  eleventyConfig.addCollection("macpedia", function (collection) {
     return collection.getFilteredByTag("macpedia").sort((a, b) => {
       return a.data.order - b.data.order;
     });
   });
 
- eleventyConfig.addCollection("winpedia", function(collection) {
+  eleventyConfig.addCollection("winpedia", function (collection) {
     return collection.getFilteredByTag("winpedia").sort((a, b) => {
       return a.data.order - b.data.order;
     });
   });
-
 
   eleventyConfig.addPassthroughCopy("assets/**/*.{jpg,png,gif,webp}");
   eleventyConfig.addPassthroughCopy("assets/MacPedia/Home/Homepage.html");
@@ -42,7 +41,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "assets",
-      output: "_site"
-    }
+      output: "_site",
+    },
   };
 };
