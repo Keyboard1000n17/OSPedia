7790b65e5f5d67a4ffee2b5c8036b1fce589652b
Keyboard1000n17 <keyboard1000n17@gmail.com>
2025-12-26 17:12:06 +0600

    Added blur behind `div.home-os-cards img.logo` on the homepage

--- _site-min/_site/assets.11tydata.js
+++
@@ -1,1 +0,0 @@
-const path = require("path"); module.exports = { permalink: (data) => { const rel = path.relative("./assets", data.page.inputPath); return rel.replace(path.extname(rel), ".html"); }, };
