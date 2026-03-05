d15aed257cf77fc3bb906d37f9774478047bd7ae
Keyboard1000n17 <keyboard1000n17@gmail.com>
2026-03-05 18:48:06 +0300

    Finish carousel components

--- assets/carousel.js
+++ assets/carousel.js
@@ -1,9 +1,23 @@
+let index = 0;
 const carousel = document.querySelector("div.carousel");
 const carouselWrapper = document.querySelector("section.carousel-wrapper");
 const carouselBackdrop = document.querySelector("div.carousel-backdrop");
 const carouselOpen = document.querySelector("button.carousel-open");
 const carouselClose = document.querySelector("img.carousel-close");
 const carouselSlides = document.querySelectorAll("div.carousel figure");
+const carouselPrev = document.querySelector(
+  "section.carousel-wrapper div.carousel-prev",
+);
+const carouselNext = document.querySelector(
+  "section.carousel-wrapper div.carousel-next",
+);
+
+function goToSlide(i) {
+  carouselSlides[i].scrollIntoView({
+    behavior: "smooth",
+    inline: "center",
+  });
+}
 
 carouselOpen.addEventListener("click", () => {
   carouselWrapper.classList.remove("hidden");
@@ -15,4 +29,16 @@ carouselClose.addEventListener("click", () => {
   carouselBackdrop.classList.add("hidden");
 });
 
-let index = 0;
+carouselPrev.addEventListener("click", () => {
+  index = index - 1 < 0 ? index + carouselSlides.length - 1 : index - 1;
+  goToSlide(index);
+});
+
+carouselNext.addEventListener("click", () => {
+  index = index + 1 > carouselSlides.length - 1 ? 0 : index + 1;
+  goToSlide(index);
+});
+
+carousel.addEventListener("scroll", () => {
+  index = Math.round(carousel.scrollLeft / carouselSlides[0].offsetWidth);
+});
